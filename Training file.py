80ed3034bda58876f4a3419310e4c184ab5a35ba
git-atoms <atom.sliwinski@gmail.com>
2021-04-12 12:50:06 +0200

    Training with variables (VAT value)

--- Training file.py
+++ Training file.py
@@ -2,7 +2,17 @@
 # wiek = 25
 # print(wiek)
 
+'''
+a = 5
+b = 3
+print(a+b)
+'''
 
-# a = 5
-# b = 3
-# print(a+b)
+VAT = 19
+cenaNettoJava = 11
+cenaNettoAjax = 19
+
+cenaBruttoJava = cenaNettoJava * (1 + VAT/100)
+cenaBruttoAjax = cenaNettoAjax * (1 + VAT/100)
+
+print(cenaBruttoAjax)
