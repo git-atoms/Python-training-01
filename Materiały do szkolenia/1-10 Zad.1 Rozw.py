eab3e15d7b6497761e235636852659a602634081
git-atoms <atom.sliwinski@gmail.com>
2021-04-22 17:47:21 +0200

    Create 1-10 Zad.1 Rozw.py

---
+++ Materiały do szkolenia/1-10 Zad.1 Rozw.py
@@ -0,0 +1,13 @@
+dlugiString = "izylidlugoiszczesliwie\
+izylidlugoiszczesliwie\izylidlugoiszczesliwie\
+izylidlugoiszczesliwieizylidlugoiszczesliwie\
+izylidlugoiszczesliwieizylidlugoiszczesliwie\
+izylidlugoiszczesliwieizylidlugoiszczesliwie\
+izylidlugoiszczesliwieizylidlugoiszczesliwie\
+izylidlugoiszczesliwieizylidlugoiszczesliwie"
+
+imie = "Karolina"
+nazwisko = "Kowalska"
+
+pelne = imie + " " + nazwisko
+
