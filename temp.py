61645be3c3c5e7f4b50078198a1c5ae322d1c0a9
git-atoms <atom.sliwinski@gmail.com>
2021-04-22 17:27:33 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,6 +1,12 @@
-num1 = 998_684_362
-num2 = 18_639_293
+# num1 = 998_684_362
+# num2 = 18_639_293
+#
+# numFull = num1 + num2
+#
+# print(numFull)
 
-numFull = num1 + num2
 
-print(numFull)
+imie = "Tomek"
+print(imie[:-1])
+
+print(imie[-1])
