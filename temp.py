f891c18a1ffd17f3b2d299e7a671a32ee82408d0
git-atoms <atom.sliwinski@gmail.com>
2021-05-11 03:49:40 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,3 +5,6 @@ print(imie[:-1]) #obcina ostatnią
 print(imie[:1]) #obcina wszystko po pierwszej
 print(imie[0]) #pokazuje tylko pierwszą
 print(imie[-1]) #pokazuje tylko ostatnią
+
+print(imie[0:]) #pokazuje wszystko od pierwszej
+print(imie[1:]) #pokazuje wszystko od drugiej
