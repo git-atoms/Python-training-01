37a56342b2e5e83dbc1358941a7d3e71b34df8b0
git-atoms <atom.sliwinski@gmail.com>
2021-05-11 03:44:25 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,7 @@
-# num1 = 998_684_362
-# num2 = 18_639_293
-#
-# numFull = num1 + num2
-#
-# print(numFull)
 
 
 imie = "Tomek"
-print(imie[:-1])
-
-print(imie[-1])
+print(imie[:-1]) #obcina ostatnią
+print(imie[:1]) #obcina wszystko po pierwszej
+print(imie[0]) #pokazuje tylko pierwszą
+print(imie[-1]) #pokazuje tylko ostatnią
