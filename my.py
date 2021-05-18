bc27f952f10603e9927f0dab5f65c58523e6536a
git-atoms <atom.sliwinski@gmail.com>
2021-05-18 04:22:28 +0200

    Example of functions
    .capitalize()
    .upper()
    .lower()

---
+++ my.py
@@ -0,0 +1,10 @@
+imie = 'tomek'
+print(imie.capitalize()) #zwiększa pierwszą literę (Tomek)
+
+
+imie = 'tomek'
+print(imie.upper()) #zwiększa wszystkie litery wyświetlanej funkcji (TOMEK)
+
+
+imie = 'TOMEK'
+print(imie.lower()) #zmniejsza wszystkie litery (tomek)
