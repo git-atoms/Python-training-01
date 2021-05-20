bde524c8b3e248ca11a13317678f045666407f98
git-atoms <atom.sliwinski@gmail.com>
2021-05-20 04:27:22 +0200

    Create 01-6 operatoryArtymetyczne.py

---
+++ Materiały do szkolenia/01-6 operatoryArtymetyczne.py
@@ -0,0 +1,16 @@
+"""
++ dodawanie
+- odejmowanie
+* mnożenie
+/ dzielenie
+
+() - jak w matematyce kierują kolejnością działania
+
+** potęgowanie
+// dzielenie w "dół"
+% - modulo (reszta z dzielenia)
+
+
+"""
+
+
