a53b4a5935f2b30577c238ce6b718a9ca51abb44
git-atoms <atom.sliwinski@gmail.com>
2021-07-05 21:13:30 +0200

    Delete Training file.py

--- Training file.py
+++
@@ -1,7 +0,0 @@
-cenaNetto = float(input("Podaj cenę netto: ").replace(',','.'))
-VAT = float(input("Podaj stawkę VAT: ").replace(',','.'))
-
-obliczonyVAT = 1 + VAT/100
-cenaBrutto = str(cenaNetto * obliczonyVAT).replace('.',',')
-
-print("Cena brutto: ", cenaBrutto)
