f92701ee070b20ad73dd917ed8136d406b4e4385
git-atoms <atom.sliwinski@gmail.com>
2021-07-09 05:14:47 +0200

    Create odcinanie znaków.py

---
+++ odcinanie znaków.py
@@ -0,0 +1,20 @@
+'''
+Poniżej bardzo dobry sposób zapisywania liczb w Pythonie
+'''
+# num1 = 998_684_362
+# num2 = 18_639_293
+#
+# numFull = num1 + num2
+#
+# print(numFull)
+
+
+
+imie = "Tomek"
+print(imie[:-1]) #obcina ostatnią (Tome)
+print(imie[:1]) #obcina wszystko po pierwszej (T)
+print(imie[0]) #pokazuje tylko pierwszą (T)
+print(imie[-1]) #pokazuje tylko ostatnią (k)
+
+print(imie[0:]) #pokazuje wszystko od pierwszej (Tomek)
+print(imie[1:]) #pokazuje wszystko od drugiej (omek)
