3284820168984514cad210db016bb339446581db
git-atoms <atom.sliwinski@gmail.com>
2021-07-09 18:56:39 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,10 +1,10 @@
 '''
 Warunek w instrukcji warunkowej jest wykonywany raz i już.
-W pętli, dopóki nie zostanie spełniony.
+W pętli jest wykonywany dopóki nie zostanie spełniony.
 '''
 
 liczba = 100
 
-while liczba >= 0:  # liczba <= 5 to "warunek"
+while liczba >= 0:  # liczba >= 0 to "warunek"
     print(liczba)
     liczba -= 1
