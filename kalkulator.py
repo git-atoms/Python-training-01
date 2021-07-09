d63cf8a7b4271981f66388a14e17a6e5e3dc5851
git-atoms <atom.sliwinski@gmail.com>
2021-07-09 18:16:31 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -6,20 +6,19 @@ if (yn == 'n'):
 elif (yn == 't'):
     print("No to zaczynamy!")
 
-print("")
 choice = input("Wybierz rodzaj działania: \n * mnożenie\n / dzielenie\n + dodawanie\n - odejmowanie\n ")
 
-a = int(input("Pierwsza wartość: "))
-b = int(input("Druga wartość: "))
+a = float(input("Pierwsza wartość: ").replace(',','.'))
+b = float(input("Druga wartość: ").replace(',','.'))
 
 if (choice == '*'):
-    print(a*b)
+    print(str(a*b).replace('.',','))
 elif (choice == '/'):
     if (b == 0):
         print("Nie dzielimy przez zero!")
     else:
-        print(a/b)
+        print(str(a/b).replace('.',','))
 elif (choice == '+'):
-    print(a+b)
+    print(str(a+b).replace('.',','))
 elif (choice == '-'):
-    print(a-b)
+    print(str(a-b).replace('.',','))
