0116ab416033e7060fa04d4b4cb04fb797e695a0
git-atoms <atom.sliwinski@gmail.com>
2021-07-12 05:16:32 +0200

    Update loop for.py

--- loop for.py
+++ loop for.py
@@ -1,6 +1,10 @@
 wynik = 0
 
+
+# "i" przechowuje przy każdym przejściu kolejny, bieżący element
+
 for i in range (0, 4):  # w nawiasie są argumenty
     x = int(input("Podaj wartość: "))
     wynik += x
+    print(i)
 print("Wynik to: ", wynik)
