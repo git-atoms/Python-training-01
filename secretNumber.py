528e279c8eea21f6395f08f408741e475ead23ae
git-atoms <atom.sliwinski@gmail.com>
2021-07-12 19:34:56 +0200

    Update secretNumber.py

--- secretNumber.py
+++ secretNumber.py
@@ -1,5 +1,6 @@
-''' Jeden gracz podaje jakąkolwiek wartość całkowitą, a drugi próbuje zgadnąć
-
+''' Jeden gracz podaje jakąkolwiek wartość całkowitą, a drugi próbuje zgadnąć.
+Jeśli będzie za dużo albo za mało to gracz jest o tym informowany.
+Gra twa, dopóki nie padnie sekretna wartość.
 '''
 
 sekretnaLiczba = int(input("Podaj sekretną liczbę: "))
