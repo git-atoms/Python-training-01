5fd61039ba533078913016b19301ea0465556f12
git-atoms <atom.sliwinski@gmail.com>
2021-09-07 06:51:00 +0200

    Update Grafy.py

--- Grafy.py
+++ Grafy.py
@@ -5,13 +5,15 @@ import numpy as np
 colors = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462',
           '#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f']
 
+edges = [['x','y'],['y','z'],['z','x']]
 G = nx.Graph()
 G.add_edges_from(edges)
 
-edges = [['x','y'],['y','z'],['z','x']]
+
 
 
 nx.draw(G, with_labels=True)
 nx.draw_networkx_edge_labels(G,edge_labels={('x','y'):'xy',\
                                             ('y','z'):'yz',\
-                                            ('z','x'):'zx')}, font_color = 'red'
+                                            ('z','x'):'zx')}
+#, font_color = 'red'
