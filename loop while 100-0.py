2cc8ddf1b872e0a5710273e7168eb23047b13750
git-atoms <atom.sliwinski@gmail.com>
2021-11-16 07:12:29 +0100

    Update loop while 100-0.py

--- loop while 100-0.py
+++ loop while 100-0.py
@@ -5,6 +5,6 @@ W pętli jest wykonywany dopóki nie zostanie spełniony.
 
 liczba = 100
 
-while liczba >= 0:  # liczba >= 0 to "warunek"
+while liczba >= 0:  # liczba >= 0 to "warunek", który mówi o tym, że dopóki nie osiągniemy 0 to pętla będzie wykonywana.
     print(liczba)
     liczba -= 1
