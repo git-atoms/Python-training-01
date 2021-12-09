a5e0795963e4f6abb1ea0504688d916c1076fe39
git-atoms <atom.sliwinski@gmail.com>
2021-12-09 04:12:17 +0100

    Update PrzetrenujTo.py

--- PrzetrenujTo.py
+++ PrzetrenujTo.py
@@ -1,2 +1,11 @@
-wynik = 0
+# wynik = 0
+
 x = int(input("Podaj liczbę całkowitą: "))
+
+if x != int:
+    print("Miała być liczba całkowita, a nie ułamek")
+else:
+    if x >= 0:
+        print(x)
+    else:
+        print(abs(x))
