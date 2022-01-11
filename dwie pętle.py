2968e6dc073780acd0ab51a16fd000bdaef37ad5
git-atoms <atom.sliwinski@gmail.com>
2022-01-11 01:45:33 +0100

    Update dwie pętle.py

--- dwie pętle.py
+++ dwie pętle.py
@@ -2,12 +2,12 @@ liczba = 0 #wartość startowa
 i = 0 #ilość cykli
 
 while liczba < 10:
-    liczba +=1
-    i +=1
+    liczba += 1
+    i += 1
     print(liczba)
 
 #kiedy z górnej pętli otrzymał pierwsze dziesięć wartości, dokłada od siebie kolejne pięć (bo tyle ma w zakresie)
 for i in range(0,5):
-    liczba +=1
-    i +=1
+    liczba += 1
+    i += 1
     print(liczba)
