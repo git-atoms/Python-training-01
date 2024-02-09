7ec4fa67ae70268dd0bb47d2983b81dd8d4abdc1
git-atoms <atom.sliwinski@gmail.com>
2024-02-09 21:18:30 +0100

    Create PIP install-upgrade.py

---
+++ PIP install-upgrade.py
@@ -0,0 +1,9 @@
+# Install or upgrade PIP in CMD window
+
+import subprocess
+
+# Uruchomienie CMD i sprawdzenie, czy pip jest zainstalowany
+subprocess.run("cmd /c python -m ensurepip", shell=True)
+
+# Aktualizacja pip
+subprocess.run("cmd /c python -m pip install --upgrade pip", shell=True)
